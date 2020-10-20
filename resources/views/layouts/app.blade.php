03a241d009ffa1ae424646757e55885197dea978
tomica95 <seleatoma@gmail.com>
2020-10-20 17:47:08 +0200

    Vue install

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,5 +14,7 @@
     <div id="app">
         @yield('content')
     </div>
+
+    <script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,30 +3,5 @@
 @section('title','Login Page')
 
 @section('content')
-    <div class="login-page flex justify-center items-center">
-        <div class="login-container">
-            <div class="login-title flex justify-center items-center text-bold">
-                Life Pal
-            </div>
-            <div class="login-subtitle flex justify-center items-center flex-wrap">
-                <span class="text-center">Welcome to your life assistant application, </span>
-                <span class="text-center text-bold font-italic font-big font-family-impact letter-spacing-3"> your Life Pal.</span>
-            </div>
-            <div class="login-form flex justify-center flex-column">
-
-                <h3 class="text-center"> Log in and keep things under control </h3>
-                <div class="form-input p-1 flex justify-center">
-                    <input type="text" name="email" placeholder="Type your email" class="radius-1">
-                </div>
-
-                <div class="form-input p-1 flex justify-center">
-                    <input type="password" name="password" placeholder="Type your password" class="radius-1">
-                </div>
-
-                <div class="form-input p-1 flex justify-center">
-                    <input type="submit" value="Log in">
-                </div>
-            </div>
-        </div>
-    </div>
+    <login-page></login-page>
 @endsection
